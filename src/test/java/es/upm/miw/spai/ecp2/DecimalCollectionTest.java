9e1a8cb34bda55247d4092d7ad5eb456d2c56a6f
Javier Combarros <Javier@138.4.192.251>
2014-10-04 12:52:24 +0200

    DecimalCollection Implementado 2

---
+++ src/test/java/es/upm/miw/spai/ecp2/DecimalCollectionTest.java
@@ -0,0 +1,35 @@
+package es.upm.miw.spai.ecp2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DecimalCollectionTest {
+
+	private DecimalCollection decimalCollection;
+	
+	@Before
+	public void before(){
+		decimalCollection = new DecimalCollection();
+		decimalCollection.add(1.0);
+		decimalCollection.add(2.0);
+		decimalCollection.add(2.5);
+	}
+
+	@Test
+	public void testSize() {
+		assertEquals(3, decimalCollection.size());
+	}
+
+	@Test
+	public void testSum() {
+		assertEquals(5.5, decimalCollection.sum(), 1e-3);
+	}
+
+	@Test
+	public void testHigher() {
+		assertEquals(2.5, decimalCollection.higher(), 1e-3);
+	}
+
+}
