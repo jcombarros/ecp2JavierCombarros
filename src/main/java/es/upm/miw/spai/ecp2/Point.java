21e78eb327207fb0bf31cdecd02fa67719a4c419
Robin Giles Ribera <robinchogiles@gmail.com>
2014-10-07 23:46:24 +0200

    Clase Point::Aadir un mtodo que permita modificar un punto

--- src/main/java/es/upm/miw/spai/ecp2/Point.java
+++ src/main/java/es/upm/miw/spai/ecp2/Point.java
@@ -42,4 +42,12 @@ public class Point {
         return "Point[" + x + "," + y + "]";
     }
     
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    
 }
